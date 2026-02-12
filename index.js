fe8ea4ce54a8144636c2b1632738f5f994f62829
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2026-02-12 15:54:22 +0700

    init project

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let homeScore = 0;
+let guestScore = 0;
+const homeScoreEl = document.getElementById('home-score');
+const guestScoreEl = document.getElementById('guest-score');
+
+function addScore(team, score) {
+  if (team === 'home') {
+    homeScore += score;
+    homeScoreEl.textContent = homeScore;
+  } else {
+    guestScore += score;
+    guestScoreEl.textContent = guestScore;
+  }
+}
+
+function resetGame() {
+  homeScore = 0;
+  guestScore = 0;
+  homeScoreEl.textContent = homeScore;
+  guestScoreEl.textContent = guestScore;
+}
